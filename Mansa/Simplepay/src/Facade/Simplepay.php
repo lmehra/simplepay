8625e002d98a41de28f7dbaa6af7db12e0da0908
lucky <lucky@mansainfotech.com>
2016-09-17 12:12:38 +0530

    remov master repo

--- Mansa/Simplepay/src/Facade/Simplepay.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Mansa\Simplepay\Facade;
- 
-use Illuminate\Support\Facades\Facade;
- 
-class Simplepay extends Facade {
- 
-    protected static function getFacadeAccessor() { }
-}
-?>
--- Mansa/Simplepay/src/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::group(['namespace' => 'Mansa\Simplepay\Controllers', 'prefix'=>'simplepaydemo'], function() {
-    // Your route goes here
-        Route::get('foo', 'SimplepayController@foo');
-});
-?>
