ebd344a055075e97614eefcf5495119f4f01314f
zero <sergey.sv5@mail.ru>
2018-05-04 15:37:22 +0600

    add reverse payment ability

--- src/Facade/Simplepay.php
+++ src/Facade/Simplepay.php
@@ -1,10 +1,12 @@
-<?php 
+<?php
 namespace Mansa\Simplepay\Facade;
- 
+
+
+
 use Illuminate\Support\Facades\Facade;
- 
+
 class Simplepay extends Facade {
- 
+
     protected static function getFacadeAccessor() { return 'simplepay';}
 }
 ?>
